1196444ed276d7b54e9f01c7d7ff974d0bcee846
jdx <216188+jdx@users.noreply.github.com>
2025-03-29 18:36:07 -0500

    fix: bug

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -1,7 +1,7 @@
 use crate::style;
 
 pub(crate) fn progress_bar(progress_current: usize, progress_total: usize, width: usize) -> String {
-    let width = width - 2;
+    let width = width.max(2) - 2;
     let progress = progress_current as f64 / progress_total as f64;
     let filled_length = (width as f64 * progress).round() as usize;
     let progress_bar = "=".repeat(filled_length) + &" ".repeat(width - filled_length);
