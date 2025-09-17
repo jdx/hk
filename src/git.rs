059f55ff64d6e7880da76353ad49b38ea7cf3177
jdx <216188+jdx@users.noreply.github.com>
2025-09-17 15:43:50 +0000

    stash: skip invoking git when selected paths have no unstaged changes

--- src/git.rs
+++ src/git.rs
@@ -556,7 +556,13 @@ impl Git {
                     .iter()
                     .any(|p| status.unstaged_modified_files.contains(p));
             }
-
+            // If nothing to stash for these paths, skip invoking git entirely
+            if !any_unstaged {
+                job.prop("message", "No unstaged changes to stash");
+                job.prop("files", &0);
+                job.set_status(ProgressStatus::Done);
+                return Ok(());
+            }
             job.prop("message", "Running git stash for selected paths");
             job.update();
             self.stash = self.push_stash(Some(paths), status, method)?;
