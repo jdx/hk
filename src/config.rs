74ddfb72dd51badae8918d5f0790a3b55958f02c
Shuai Zhang <zhangshuai.ustc@gmail.com>
2025-11-18 14:26:00 -0800

    fix: Configuration Read Support YML File Extension

--- src/config.rs
+++ src/config.rs
@@ -23,7 +23,7 @@ impl Config {
                 let raw = xx::file::read_to_string(path)?;
                 toml::from_str(&raw)?
             }
-            "yaml" => {
+            "yaml" | "yml" => {
                 let raw = xx::file::read_to_string(path)?;
                 serde_yaml::from_str(&raw)?
             }
