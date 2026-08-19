3bd04b8d9837baf7ba2e1721ded1dce3936d7407
default <216188+jdx@users.noreply.github.com>
2026-08-19 02:29:23 +0000

    refactor: add usage 6 adoption experiment

---
+++ experiments/usage6/target/debug/deps/shadow_hk-5cbba16e4db86bb8.d
@@ -0,0 +1,5 @@
+/home/coder/src/hk-usage6/experiments/usage6/target/debug/deps/shadow_hk-5cbba16e4db86bb8.d: src/lib.rs
+
+/home/coder/src/hk-usage6/experiments/usage6/target/debug/deps/libshadow_hk-5cbba16e4db86bb8.rmeta: src/lib.rs
+
+src/lib.rs:
